983af20b84f574ea00556b9c872dc7b3b5095ac9
kootaekmin <kootaekmin@gmail.com>
2021-05-03 20:35:54 +0900

    final

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,19 @@ import _ from 'lodash';
 
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+
+	// const userIds = _.uniq(_.map(getState().posts, 'userId'));
+	// userIds.forEach((id) => dispatch(fetchUser(id)));
+
+	_.chain(getState().posts)
+		.map('userId')
+		.uniq()
+		.forEach((id) => dispatch(fetchUser(id)))
+		.value();
+};
+
 export const fetchPosts = () => async (dispatch) => {
 	const response = await jsonPlaceholder.get('/posts');
 
@@ -13,3 +26,12 @@ export const fetchUser = (id) => async (dispatch) => {
 
 	dispatch({ type: 'FETCH_USER', payload: response.data });
 };
+
+// using lodash
+//
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+// 	const response = await jsonPlaceholder.fet(`/users/${id}`);
+
+// 	dispatch({type: 'FETCH_USER', payload: response.data})
+// });
